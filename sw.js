810fc59ea6847c52eba881a156576a85ddbdbf29
Diego Sozio <diego.sozio92@gmail.com>
2026-06-29 12:08:35 +0200

    fix image icon

--- sw.js
+++ sw.js
@@ -38,8 +38,16 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    }),
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request)),
   );
 });
